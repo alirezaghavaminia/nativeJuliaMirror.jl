e2dc4b6f7f456618999fa942292a3b5905811700
AlirezaNHPCC <alirezamecheng@gmail.com>
2020-04-27 16:33:59 +0430

    improving Error handling and comments in the Input section

--- modules/check_inputs.jl
+++ modules/check_inputs.jl
@@ -1,48 +1,211 @@
-function input(op,key,err;def,req = true)
+"""
+input(op, key, err; def, req = true)
+
+checks the inputs for errors and assign the value if possible
+"""
+function input(op, key, err; def, req = true)
     type = typeof(def)
-    get(op,key) do
+    @debug("    >>",key)
         if req # in case req
+            @debug("============ Required Value")
+
             if haskey(op,key) # in case defined
-                if type == typeof("")
-                    if op[key] ==""
+                @debug("============ Key is defined")
+                
+                if type == typeof("") # case String
+                    
+                    @debug("============ Type: String")
+                    if op[key] == ""
                         push!(err,key => "String")
+                        @debug("============ Input is not valid, pushing error")
                     end
-                elseif type == typeof(true)
+                
+                elseif type == typeof(true) # case Boolean
+                    
+                    @debug("============ Type Boolean")
                     if !(op[key] == true || op[key] == false)
                         push!(err,key => "Boolean")
+                        @debug("============ Input is not valid, pushing error")
                     end
-                end # end typeof  
+
+                end # end typeof
+
+                @debug("============ Returning value anyways")
                 return op[key]
+
             else # in case not defined (commented)
+
+                @debug("============ Key is not defined, pushing error, returning Default")
                 push!(err,key => repr(type))
                 return def
+
             end # end haskey
+
         else # in case not req
+            @debug("============ NOT Required")
+
             if haskey(op,key)
+                @debug("============ The key is defined. Returning the value")
                 return op[key]
             else             
+                @debug("============ The key is NOT defined. Returning the Default")
                 return def
             end
         end
-    end # end get 
 end
 
-function report_errors(error_dict; fatal = true, path = homedir(), name = "Julia_repo_make_ERRORS.log")
-    if !isempty(error_dict)
-        path = joinpath(path,name)
-        for (key,val) in error_dict
-            println("ERROR in \"configuration.toml\". You must provide a valid ",repr(val)," input to: ", repr(key), " option.")
-            flush(stdout)
+"""
+report_errors(error_dict; fatal = true, path = homedir(), name = "Julia_repo_make_ERRORS.log", msg = false)
+
+reports the Errors which are in the error_dict in the stdout and write them into a report file in the path 
+"""
+function report_errors(error_dict; fatal = true, path = homedir(), name = "Julia_repo_make_ERRORS.log", msg = false)
+    mkpath(path)
+    path = joinpath(path,name)
+    if !msg
+        if !isempty(error_dict)
+            for (key,val) in error_dict
+                println("ERROR in the \"configuration.toml\". You must provide a valid ",repr(val)," input to: ", repr(key), " option.")
+                flush(stdout)
+            end
+            file = open(path,"w")
+            for (key,val) in error_dict
+                println(file,"ERROR in the \"configuration.toml\". You must provide a valid ",repr(val)," input to: ", repr(key), " option.")
+                flush(file)
+            end
+            close(file)
+            println("Error report is saved at: ", repr(path))
+            if fatal 
+                exit(1)
+                println(" === THAT WAS FATAL")
+
+            end
         end
+    else
         file = open(path,"w")
         for (key,val) in error_dict
-            println(file,"ERROR in \"configuration.toml\". You must provide a valid ",repr(val)," input to: ", repr(key), " option.")
+            println(file,error_dict[key])
             flush(file)
+            println(error_dict[key])
+            flush(stdout)
         end
         close(file)
-        println("Errors are saved at: ", repr(path))
-        if fatal 
+        println("Error report is saved at: ", repr(path))
+        if fatal
             exit(1)
+            println(" === THAT WAS FATAL")
         end
+    end
+
+end
+
+"""
+parse_configuration(path)
+
+Parse the TOML file located in the path and reports errors if there are any.
+"""
+function parse_configuration(path)
+    err = Dict{String,String}()
+    result =    try 
+                    Pkg.TOML.parsefile(path)
+                catch e
+                    massage = "ERRPR: Parsing configuration file faild. Check this file for syntax errors."
+                    push!(err,"configuration_file_format" => massage)
+                end
+        if !isempty(err)
+            report_errors(err,fatal = true, msg = true)
+        end
+        return result
+end
+
+"""
+check_prefix(path)
+
+checks the path to see if its is a valid writable path or not. If the path is valid it creates the path. 
+"""
+function check_prefix(path)
+    error_dict = Dict{String,String}()
+    if path == ""
+        @debug("============ PREFIX IS NOT A VALID STRING")
+        push!(error_dict,"prefix" => "The value of \"prefix\" key in the \"configuration.toml\" file is not correct.")
+        report_errors(error_dict,fatal = true, msg = true)
+    end
+    if isdir(path)
+        @debug("============ PREFIX IS A PATH")
+        try 
+            mkdir(joinpath(path,"test"))
+            rm(joinpath(path,"test"),force = true, recursive = true)
+        catch e
+            massage = "Permission denied. Can't write into the prefix."
+            push!(error_dict,"prefix" => massage)
+        end
+    else
+        @debug("============ PREFIX IS NOT A PATH")
+        try 
+            mkdir(path)
+        catch e
+            massage = "ERROR: ", repr(path), " is not a \"Writable\" directory. Please fix the \"prefix\" value in the \"configuration.toml\""
+            println(massage)
+            push!(error_dict,"prefix" => massage,msg = true)
+        end
+    end
+    if !isempty(error_dict)
+        report_errors(error_dict,fatal = true, msg = true)
+    end
+end
+
+"""
+check_uuid!(id, error_dict)
+
+checks the id to see if it is a valid UUID and returns the UUID object if id is valid.
+"""
+function check_uuid!(id, error_dict)
+    err = []
+    result = try
+        UUIDs.UUID(id)
+    catch e
+        @debug("============ UUID STRING IS NOT VALID, pushing error ")
+        push!(err,e)
+    end
+    if isempty(err)
+        return UUIDs.UUID(id)
+    else
+        push!(error_dict,"new_registry_uuid" => "UUID")
+    end
+end
+
+
+"""
+check_configuration_file(configuration)
+
+Checks the configuration file to see if it has proper keys or not. 
+"""
+function check_configuration_file(configuration)
+    error_dict = Dict{String,String}()
+
+    if !haskey(configuration,"include")
+        println("============ There is no include in configuration")
+        massage = "ERROR: The \"configuration.toml\" file has no key \"[include]\"."
+        push!(error_dict,"include" => massage)
+
+    else # when configuration has include key
+        if !haskey(configuration["include"],"registries")
+            massage = "ERROR: In the \"configuration.toml\" file \"[include]\" has no key \"registries\"."
+            push!(error_dict,"registries" => massage)
+        else # when include has a registries key 
+            if size(configuration["include"]["registries"],1) < 1
+                massage = "ERROR: In the \"configuration.toml\" file, You must specify at least one registy url."
+                push!(error_dict,"registries_number" => massage)
+            end
+        end
+    end
+
+    if !haskey(configuration,"options")
+        massage = "ERROR: The \"configuration.toml\" file has no key \"[options]\"."
+        push!(error_dict,"options" => massage)
+    end
+
+    if !isempty(error_dict)
+        report_errors(error_dict, fatal = true, msg = true)    
     end
 end
--- modules/inputs.jl
+++ modules/inputs.jl
@@ -1,22 +1,25 @@
 include("check_inputs.jl")
 
-# check for input errors
-input_errors = Dict{String,String}()
-@info("== parsing \"configuration.toml\"...")
-configuration = Pkg.TOML.parsefile(joinpath(project_root,"configuration.toml"))
-@info("    <> Done.")
+println("== parsing \"configuration.toml\"...")
+configuration = parse_configuration(joinpath(project_root,"my_configuration.toml"))
+check_configuration_file(configuration)
 
 op = configuration["options"]
 
+# prefix input 
+temp_errors = Dict{String,String}()
+temp_prefix = input(op,"prefix", temp_errors, def = "")
+check_prefix(temp_prefix)
+cd(temp_prefix)
+const PREFIX = pwd() # making prefix an absolute address
+cd(user_original_directory)
 
+# other input
+input_errors = Dict{String,String}()
+const MY_REGISTRY_UUID          = input(op,"new_registry_uuid",   input_errors, def = "")
+const MY_REGISTRY_UUID_OBJECT   = check_uuid!(MY_REGISTRY_UUID,    input_errors)
 const DOWNLOAD_ALL_PACKAGES     = input(op,"download_all_packages",input_errors, def = true)
 const MY_REGISTRY_NAME          = input(op,"new_registry_name",   input_errors, def = "")
-const MY_REGISTRY_UUID          = input(op,"new_registry_uuid",   input_errors, def = "")
-const MY_REGISTRY_UUID_OBJECT   = UUIDs.UUID(MY_REGISTRY_UUID)
-const TMP_TARGET                = input(op,"prefix",              input_errors, def = "")
-cd(TMP_TARGET)
-const PREFIX = pwd()
-cd(user_original_directory)
 const MY_GIT_USER_NAME          = input(op,"git_user_name",       input_errors, def = "")
 const MY_GIT_USER_EMAIL         = input(op,"git_user_email",      input_errors, def = "")
 const MY_INFO                   = input(op,"your_info",           input_errors, def = "",    req = false)
@@ -28,6 +31,7 @@ const REFRESH_CONF_FILES        = input(op,"refresh_conf_files",  input_errors, 
 const MAKE_BACKUPS              = input(op,"make_backups",        input_errors, def = true,  req = false)
 const CLEAN_INSTALLATION        = input(op,"clean_installation",  input_errors, def = false, req = false)
 
+# network inputs
 const INSTALL_ON_NETWORK        = input(op,"install_on_network",  input_errors, def = true,  req = false)
 if INSTALL_ON_NETWORK
     const NETWORK_PREFIX = input(op,"network_access_address",input_errors, def = "", req = true)
@@ -58,12 +62,14 @@ const DATA_DIR = "META_DATA"
 const REPO_PATH_EXTENTION = ".git"
 const LOCAL_REG_DIR = "local_access"
 const PUBLIC_REG_DIR = "public_access"
+const ERRORS_DIR = "configuration_errors"
 # const REPO = "repo"
 # const REG = "registries"
 const PACKAGES_PATH          = joinpath(PREFIX,PACKAGES_DIR)
 const NEW_REG_PATH           = joinpath(PREFIX,NEW_REG_DIR)
 const BUILD_INFORMATION_PATH = joinpath(PREFIX,BUILD_INFORMATION_DIR)
 const META_DATA_PATH         = joinpath(PREFIX,BUILD_INFORMATION_DIR,DATA_DIR)
+const ERRORS_PATH            = joinpath(PREFIX,BUILD_INFORMATION_DIR,ERRORS_DIR)
 const SRC_PATH               = joinpath(PREFIX,SRC_DIR)
 const SP_REG_PATH            = joinpath(PREFIX,SRC_DIR,REG_DIR)
 const SP_PROJECTS_PATH       = joinpath(PREFIX,SRC_DIR,PROJECTS_DIR)
@@ -71,7 +77,5 @@ const SP_MANIFESTS_PATH      = joinpath(PREFIX,SRC_DIR,MANIFESTS_DIR)
 const KEEP_ONLINE_PATH       = joinpath(PREFIX,SRC_DIR,KEEP_ONLINE_DIR)
 const REMOVE_FROM_REPO_PATH  = joinpath(PREFIX,SRC_DIR,REMOVE_FROM_REPO_DIR)
 
-report_errors(input_errors)
-
-
-
+# reporting the errors.
+report_errors(input_errors,path = ERRORS_PATH)
